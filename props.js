7aa0c46ec2a215b0d3f37e7baf1f658c522053ca
Christof Schwarz <christof.schwarz@databridge.ch>
2021-11-12 09:05:24 +0100

    Update props.js

--- props.js
+++ props.js
@@ -4,7 +4,7 @@ define(["jquery", "./functions"], function ($, functions) {
 
     var pickList = [];
     var pickList2 = [];
-
+    const ext = 'db_ext_guided_tour';
     const bgColorPickers = '#079B4A';
 
     function subSection(labelText, itemsArray) {
@@ -12,12 +12,16 @@ define(["jquery", "./functions"], function ($, functions) {
             component: 'expandable-items',
             items: {}
         };
-		const labelHash = btoa(labelText).split('=')[0];
-		ret.items[labelHash] = {
-			label: labelText,
-			type: 'items',
-			items: itemsArray
-		};
+		var hash = 0;
+        for (var j = 0; j < labelText.length; j++) {
+            hash = ((hash << 5) - hash) + labelText.charCodeAt(j)
+            hash |= 0;
+        }
+        ret.items[hash] = {
+            label: labelText,
+            type: 'items',
+            items: itemsArray
+        };
         return ret;
     }
 
@@ -43,6 +47,19 @@ define(["jquery", "./functions"], function ($, functions) {
         const interSort = arg.qHyperCubeDef.qInterColumnSortOrder.length > 0 ? (arg.qHyperCubeDef.qInterColumnSortOrder[0] == 0) : false;
         //console.log('qSortCriterias', arg.qHyperCubeDef.qInterColumnSortOrder[0], sortByLoadOrder, interSort);
         return !sortByLoadOrder || !interSort;
+    }
+
+    async function resolveProperty(prop, enigma) {
+        // takes care of a property being either a constant or a expression, which needs to be evaluated
+        var ret;
+        if (prop.qStringExpression) {
+            ret = await enigma.evaluate(prop.qStringExpression.qExpr);
+            //console.log('was expression: ', ret);
+        } else {
+            //console.log(prop,' was constant');
+            ret = prop;
+        }
+        return ret;
     }
 
     return {
@@ -140,117 +157,166 @@ define(["jquery", "./functions"], function ($, functions) {
                         })
 
                     }
-                }, subSection('Select A Specific Tour', {
-                    e:{
+                }, subSection('Select A Specific Tour', [
+                    {
                         label: "If you have multiple tours in your data model, you may want to filter the right one by making below selection",
                         component: "text"
-                    }, f:{
+                    }, {
                         label: 'Select in field',
                         type: 'string',
                         ref: 'pTourField',
                         expression: 'optional'
-                    }, g:{
+                    }, {
                         label: 'Select this value',
                         type: 'string',
                         ref: 'pTourSelectVal',
                         expression: 'optional'
                     }
-                }), subSection('Button Text & Color', {
-                    a:{
+                ]), subSection('Button Text & Color', [
+                    {
                         label: 'Text for Tour Start',
                         type: 'string',
                         ref: 'pTextStart',
                         defaultValue: 'Start Tour',
                         expression: 'optional'
-                    }, b:{
+                    }, {
                         type: "boolean",
                         defaultValue: true,
                         ref: "pShowIcon",
                         label: "Show play icon"
-                    }, c:{
+                    }, {
                         label: 'Background-color of button',
                         type: 'string',
                         ref: 'pExtensionBgColor',
                         expression: 'optional'
-                    }, d:{
+                    }, {
                         label: 'Font-color of button',
                         type: 'string',
                         ref: 'pExtensionFontColor',
                         expression: 'optional'
+                    }, {
+                        label: 'More styling',
+                        type: 'string',
+                        ref: 'pMoreStyles',
+						defaultValue: 'font-size:large;',
+                        expression: 'optional'
                     }
-                }), subSection('Tooltips Texts & Colors', {
-                    h:{
+                ]), subSection('Tooltips Texts & Colors', [
+                    {
                         label: 'Text for Next button',
                         type: 'string',
                         ref: 'pTextNext',
                         defaultValue: 'Next',
                         expression: 'optional'
-                    }, i:{
+                    }, {
                         label: 'Text for Done button',
                         type: 'string',
                         ref: 'pTextDone',
                         defaultValue: 'Done',
                         expression: 'optional'
-                    }, j:{
+                    }, {
                         label: 'Default tooltip backgr-color',
                         type: 'string',
                         ref: 'pBgColor',
                         defaultValue: 'rgba(0,0,0,0.9)',
                         expression: 'optional'
-                    }, k:{
+                    }, {
                         label: 'Dynamic backgr-color from dim',
                         component: "dropdown",
                         ref: "pBgColorFromDim",
                         defaultValue: "",
                         options: function (arg) { return getDimNames(arg); }
-                    }, l:{
+                    }, {
                         label: 'Default tooltip font color',
                         type: 'string',
                         ref: 'pFontColor',
                         defaultValue: '#e0e0e0',
                         expression: 'optional'
-                    }, m:{
+                    }, {
                         label: 'Dynamic font-color from dim',
                         component: "dropdown",
                         ref: "pFontColorFromDim",
                         defaultValue: "",
                         options: function (arg) { return getDimNames(arg); }
                     }
-                }), subSection('Advanced Settings', {
-                    n:{
+                ]), subSection('Auto-launch Tour \u2605', [
+                    {
+                    label: "These settings apply only if you have a licensed version.",
+                    component: "text"
+                }, {
+                        label: 'Automatically show this tour',
+                        type: 'string',
+						component: 'dropdown',
+                        ref: 'pAutoLaunch',
+						defaultValue: 'no',
+						options: [{
+							value: "no",
+							label: "No auto-launch"
+						}, {
+							value: "once",
+							label: "Launch once per user"
+						},{
+							value: "always",
+							label: "Always auto-launch"
+						}]
+                    }, {
+                        label: 'Relaunch once after',
+                        type: 'string',
+                        ref: 'pRelaunchAfter',
+                        defaultValue: '189912312359',
+                        expression: 'optional',
+						show: function(arg) { return arg.pAutoLaunch == 'once'}
+                    }, {
+						label: "Format: YYYYMMDDhhmm",
+						component: "text",
+						show: function(arg) { return arg.pAutoLaunch == 'once'}
+					}, {
+						label: function(arg) { return 'Saved settings: ' + window.localStorage.getItem(app.id + '|' + arg.qInfo.qId) },
+						component: "text",
+						show: function(arg) { return arg.pAutoLaunch == 'once'}
+					}, {
+						label: "Clear saved settings",
+						component: "button",
+						action: function (arg) {
+							window.localStorage.removeItem(app.id + '|' + arg.qInfo.qId);
+							functions.leonardoMsg(arg.qInfo.qId,'Success','Removed local item',null,'OK');
+						}
+					}
+                ]), subSection('Advanced Settings', [
+                    {
                         label: 'Font Size',
                         type: 'string',
                         ref: 'pFontSize',
                         defaultValue: '11pt',
                         expression: 'optional'
-                    }, o:{
+                    }, {
                         label: 'Default tooltip width (px)',
                         type: 'number',
                         ref: 'pDefaultWidth',
                         defaultValue: 250,
                         expression: 'optional'
-                    }, p:{
+                    }, {
                         label: 'Dynamic tooltip width from dim',
                         component: "dropdown",
                         ref: "pWidthFromDim",
                         defaultValue: "",
                         options: function (arg) { return getDimNames(arg); }
-                    }, q:{
+                    }, {
                         label: 'Opacity of inactive objects',
                         type: 'number',
                         ref: 'pOpacity',
                         component: "slider",
-						defaultValue: 0.1,
+                        defaultValue: 0.1,
                         min: 0.1,
-						max: 1,
-						step: 0.1
-                    }, r:{
+                        max: 1,
+                        step: 0.1
+                    }, {
                         label: 'Offset when top (px)',
                         type: 'number',
                         ref: 'pOffsetTop',
                         defaultValue: 10,
                         expression: 'optional'
-                    }, s:{
+                    }, {
                         label: 'Offset when left (px)',
                         type: 'number',
                         ref: 'pOffsetLeft',
@@ -263,41 +329,58 @@ define(["jquery", "./functions"], function ($, functions) {
                         defaultValue: '#qv-page-container',
                         expression: 'optional'
                     }*/
-                })
+                ]), {
+                    label: "\u2605 Premium feature only with license",
+                    component: "text"
+                }
             ]
         },
 
         licensing: function (app) {
+            const enigma = app.model.enigmaModel;
             return [
                 {
-                    type: "array",
-                    ref: "licenses",
-                    label: "List Items",
-                    itemTitleRef: "label",
-                    allowAdd: true,
-                    allowRemove: true,
-                    addTranslation: "Add License",
-                    items: [
-                        {
-                            type: "string",
-                            ref: "label",
-                            label: "Domain"
-                        }, {
-                            type: "number",
-                            ref: "pLicNo",
-                            label: "License"
-                        }, {
-                            type: "number",
-                            ref: "pCheckSum",
-                            label: "Check Sum"
-                        }, {
-                            label: function (arg) {
-                                const isLicensed = functions.isLicensed(arg.pLicNo, arg.pCheckSum, arg.label.length, arg.label);
-                                return isLicensed ? (String.fromCharCode(0x2713) + ' licensed') : (String.fromCharCode(0x274C) + 'not licensed')
-                            },
-                            component: 'text'
-                        }
-                    ]
+                    type: "string",
+                    ref: "pLicenseJSON",
+                    label: "License String",
+                    component: "textarea",
+                    rows: 5,
+                    maxlength: 4000,
+                    expression: 'optional'
+                }, {
+                    label: "Contact data/\\bridge",
+                    component: "link",
+                    url: 'https://www.databridge.ch/contact-us'
+                }, {
+                    label: "Check License",
+                    component: "button",
+                    action: function (arg) {
+
+                        const ownId = arg.qInfo.qId;
+                        resolveProperty(arg.pLicenseJSON, enigma).then((lstr) => {
+                            console.log('License String', lstr);
+                            var report = '';
+                            try {
+                                const j = JSON.parse(lstr);
+                                console.log('License JSON', j);
+                                for (const d in j) {
+                                    const applicable = d == functions.patternize(location.hostname, d);
+                                    const m = functions.hm(d, ext);
+                                    r = functions.isLicensed(d, j[d][0], j[d][1]);
+                                    report += (`<tr><td>${d}</td><td>${applicable}</td><td>${j[d][0]}</td><td>${j[d][1]}</td><td>${r}</td></tr>`);
+                                }
+								if (report=='')
+									functions.leonardoMsg(ownId, 'Error', "This isn't a valid license.", null, 'OK')
+								else
+                                	functions.leonardoMsg(ownId, 'Result',
+                                    '<table><tr style="text-align:left;"><th>Domain</th><th>Applies?</th><th>License No.</th><th>CheckSum</th><th>Valid?</th></tr>'
+                                    + report + '</table>', null, 'OK');
+                            }
+                            catch (err) {
+                                functions.leonardoMsg(ownId, 'Error', "This isn't a valid license.", null, 'OK');
+                            };
+                        });
+                    }
                 }
             ]
         },
@@ -309,10 +392,10 @@ define(["jquery", "./functions"], function ($, functions) {
                     component: "link",
                     url: '../extensions/db_ext_guided_tour/db_ext_guided_tour.qext'
                 }, {
-                    label: "This extension is free of charge by data/\\bridge, Qlik OEM partner and specialist for Mashup integrations.",
+                    label: "This extension is available either licensed or free of charge by data/\\bridge, Qlik OEM partner and specialist for Mashup integrations.",
                     component: "text"
                 }, {
-                    label: "Use as is. No support without a maintenance subscription.",
+                    label: "Without license you may use it as is. Licensed customers get support.",
                     component: "text"
                 }, {
                     label: "",
